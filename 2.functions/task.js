678d507c4c57966625b1322b003a5da23a72e3a5
Ivan <tenekrut30@gmail.com>
2024-01-01 22:22:45 +0300

    Второе задание_проблема_1

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,7 +1,13 @@
 function getArrayParams(...arr) {
-
-  return { min: min, max: max, avg: avg };
+  let min, max, sum, avg;
+  min = Math.min(...arr);
+  max = Math.max(...arr);
+  sum = arr.reduce((acc, c) => acc + c);
+  avg = (sum / arr.length).toFixed(2);
+  avg = (Number(avg));
+  return {min:min, max:max, avg:avg};
 }
+
 
 function summElementsWorker(...arr) {
 
