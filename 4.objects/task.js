f72aab11aebfff84a420889607e2565b7c384eb3
Ivan <tenekrut30@gmail.com>
2024-01-24 16:13:55 +0300

    Домашка по объектам  на проверку_1

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,24 @@
 function Student(name, gender, age) {
-  
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+  this.marks = [];
 }
 
-Student.prototype.setSubject = function (subjectName) {
-  
-}
+let student_1 = new Student("Ivan", "male", "18");
+let student_2 = new Student("Andrey", "male", "19");
+let student_3 = new Student("Lena", "woman", "20");
 
-Student.prototype.addMarks = function (...marks) {
-  
-}
 
-Student.prototype.getAverage = function () {
-  
-}
+Student.prototype.setSubject = function(subjectName) {
+  this.subject = subjectName;
+  this.marks = [];
+};
 
-Student.prototype.exclude = function (reason) {
-  
-}
+Student.prototype.addMarks = function(...marks) {
+  if (this.hasOwnProperty('marks')) {
+    this.marks.push(...marks);
+  } else {
+    return 0;
+  };
+};
